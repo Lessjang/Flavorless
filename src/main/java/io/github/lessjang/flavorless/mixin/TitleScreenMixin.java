7a16efa726e84891cec62808f9818dd56bbfbf34
Caroline Bell <carolinejoybell@gmail.com>
2021-10-24 19:08:40 -0700

    Change branding

--- src/main/java/io/github/lessjang/flavorless/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/lessjang/flavorless/mixin/TitleScreenMixin.java
@@ -1,6 +1,5 @@
-package net.fabricmc.example.mixin;
+package io.github.lessjang.flavorless.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +7,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+public class TitleScreenMixin {
+	@Inject(method = "init", at = @At("HEAD"))
+	private void onInit(CallbackInfo ci) {
+		System.out.println("Hello world!");
 	}
 }
