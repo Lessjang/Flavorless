3fd07142d1690da099cca67a1a6763bd59c07b75
Falkreon <falkreon@gmail.com>
2021-10-26 10:34:34 -0500

    Clear block registry, fix CME in TitleScreenMixin

--- src/main/java/io/github/lessjang/flavorless/client/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/lessjang/flavorless/client/mixin/TitleScreenMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-@Environment(EnvType.CLIENT)
+//@Environment(EnvType.CLIENT)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("HEAD"))
 	private void onInit(CallbackInfo ci) {
---
+++ src/main/java/io/github/lessjang/flavorless/mixin/BlocksMixin.java
@@ -0,0 +1,64 @@
+package io.github.lessjang.flavorless.mixin;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.AirBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.gen.carver.CarverConfig;
+import net.minecraft.world.gen.carver.CarverDebugConfig;
+import net.minecraft.world.gen.carver.CaveCarverConfig;
+import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilders;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+
+@Mixin({Blocks.class , /*SurfaceBuilder.class, ConfiguredSurfaceBuilders.class, CarverDebugConfig.class, CarverConfig.class, CaveCarverConfig.class*/})
+public class BlocksMixin {
+	
+	@Inject(method = "<clinit>", at = @At("HEAD"), cancellable=true)
+	private static void mixin_clinit(CallbackInfo info) {
+		Block voidBlock = new Block(FabricBlockSettings.of(Material.AIR).noCollision().dropsNothing().air()) {
+			public BlockRenderType getRenderType(BlockState state) {
+				return BlockRenderType.INVISIBLE;
+			}
+
+			public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+				return VoxelShapes.empty();
+			}
+		};
+		
+		Block unvoidBlock = new Block(FabricBlockSettings.of(Material.STONE).strength(-1.0F, 3600000.0F).dropsNothing().allowsSpawning((state, view, pos, entType)->false));
+		
+		for(Field f : Blocks.class.getDeclaredFields()) {
+			//match "static Block" fields
+			if (!Modifier.isStatic(f.getModifiers())) continue;
+			if (!f.getType().equals(Block.class)) continue;
+			
+			//try {
+				//f.setAccessible(true);
+				//f.set(null, voidBlock);
+				//f.setAccessible(false);
+			//} catch (IllegalArgumentException | IllegalAccessException e) {
+			//	e.printStackTrace();
+			//}
+		}
+		
+		info.cancel();
+	}
+}
